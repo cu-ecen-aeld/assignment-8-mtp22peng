1b98603780bb7ed8c333dce0373996070eca6f25
mtp22 <mt.peng@gmail.com>
2023-10-19 08:58:09 +0800

    ok

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -1,20 +1,22 @@
+
 ##############################################################
 #
-# LDD
+# AESDCHAR
 #
 ##############################################################
 
-LDD_VERSION = 718cbdf07e082486e008537660e6b9fb4fb07a45
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+
+AESDCHAR_VERSION = 965928d91dea6c792a00705e34aa1cfe731a305d
+
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-mtp22peng.git'
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-mtp22peng.git'
 
-LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
-LDD_LICENSE = GPL-2.0
-LDD_LICENSE_FILES = COPYING
-LDD_MODULE_SUBDIRS = misc-modules
-LDD_MODULE_SUBDIRS += scull
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
